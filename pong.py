788dc5004defbe79d9de88656104d97ee4df91d2
lucasluize-tech <lucasluize.tech@gmail.com>
2020-09-08 21:44:13 -0300

    Finished tutorial part 1

--- pong.py
+++ pong.py
@@ -6,27 +6,69 @@ from kivy.clock import Clock
 from kivy.lang import Builder
 
 
-class PongGame(Widget):
-    kv = Builder.load_file("pong.kv")
+class PongPaddle(Widget):
+    score = NumericProperty()
+
+    # Bouncing the ball when touches the paddle with .collide_widget(ball)
+    def bounce(self, ball):
+        if self.collide_widget(ball):
+            speedup = 1.1
+            offset = 0.1 * Vector(0, ball.center_y - self.center_y)
+            ball.velocity = speedup * (offset - ball.velocity)
 
 
 class PongBall(Widget):
-    velocity_x = NumericProperty(0)
-    velocity_y = NumericProperty(0)
+    velocity_x = NumericProperty()
+    velocity_y = NumericProperty()
     velocity = ReferenceListProperty(velocity_x, velocity_y)
 
+    # how to move the ball
     def move(self):
         self.pos = Vector(*self.velocity) + self.pos
 
 
-class PongPaddle(Widget):
-    pass
+class PongGame(Widget):
+    ball = ObjectProperty()
+    player1 = ObjectProperty()
+    player2 = ObjectProperty()
+
+    # kickoff give a vel to move()
+    def serve(self, vel=(4, 0)):
+        self.ball.center = self.center
+        self.ball.velocity = vel
+    
+    def update(self, frame):
+        self.ball.move()
+        self.player1.bounce(self.ball)
+        self.player2.bounce(self.ball)
+
+        # Ball bouncing from TOP and BOTTOM
+        if (self.ball.y < 0) or (self.ball.top > self.height):
+            self.ball.velocity_y *= -1
+
+        # Keeping Scores
+        if self.ball.x < self.x:
+            self.player2.score += 1
+            self.serve(vel=(4, 0))
+        if self.ball.x > self.width:
+            self.player1.score += 1
+            self.serve(vel=(-4, 0))
+
+    # Move Paddles
+    def on_touch_move(self, touch):
+        if touch.x < self.width/2:
+            self.player1.center_y = touch.y
+        if touch.x > self.width - self.width/2:
+            self.player2.center_y = touch.y
 
 
 class PongApp(App):
-    def Build(self):
-        return PongGame
+    def build(self):
+        game = PongGame()
+        game.serve()
+        Clock.schedule_interval(game.update, 1.0/60.0)
+        return game
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     PongApp().run()
